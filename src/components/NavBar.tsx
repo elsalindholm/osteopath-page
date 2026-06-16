cf0a7645396a71fe8ed955542e2c8f78ec88bde6
MNewbigging <31487907+elsalindholm@users.noreply.github.com>
2026-06-16 21:16:12 +0100

    mobile nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,49 @@
+import { useState } from "react";
 import "../assets/scss/navbar.scss";
 
+const navLinks = [
+  { href: "#minusta", label: "Minusta" },
+  { href: "#osteopatia", label: "Osteopatia" },
+  { href: "#varaa-aika", label: "Varaa aika" },
+  { href: "#yhteystiedot", label: "Yhteystiedot" },
+];
+
 const NavBar = () => {
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
   return (
     <div className="navbar-container">
       <div>Logo goes here</div>
       <div className="nav-link-container">
-        <a className="nav-link" href="#minusta">Minusta</a>
-        <a className="nav-link" href="#osteopatia">Osteopatia</a>
-        <a className="nav-link" href="#varaa-aika">Varaa aika</a>
-        <a className="nav-link" href="#yhteystiedot">Yhteystiedot</a>
+        {navLinks.map(({ href, label }) => (
+          <a key={href} className="nav-link" href={href}>
+            {label}
+          </a>
+        ))}
       </div>
+      <button
+        className="burger-menu-button"
+        onClick={() => setMobileNavOpen(!mobileNavOpen)}
+      >
+        <div className="burger-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </button>
+      {mobileNavOpen && (
+        <div className="mobile-nav-container">
+          {navLinks.map(({ href, label }) => (
+            <a
+              key={href}
+              className="nav-link"
+              href={href}
+              onClick={() => setMobileNavOpen(false)}
+            >
+              {label}
+            </a>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
